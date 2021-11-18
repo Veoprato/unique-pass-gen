cbff6e0eafa9b8c2f09a6ccc90eeb444bf8e08b1
Veoprato <75872616+Veoprato@users.noreply.github.com>
2021-11-18 05:12:02 -0500

    added confirmation for password criteria

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,22 @@
 // Assignment Code
-
 function generatePassword(){
+  var passwordLength = "";
+  passwordLength = window.prompt("How many characters would you like your password to be? (Must be 8-128 Characters)");
+    if(passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)){
+      window.alert("Not a valid character count! Please try again.");
+      generatePassword();
+    } else {
+      confirmUppercase = window.confirm("Click OK to include uppercase letters.");
+      confirmLowercase = window.confirm("Click OK to include lowercase letters.");
+      confirmNumber = window.confirm("Click OK to include numbers.");
+      confirmSpecial = window.confirm("Click OK to include special characters.");
+  
+      if(confirmUppercase===false && confirmLowercase===false && confirmNumber===false && confirmSpecial===false) {
+        window.alert("Please select at least one condition for password.");
+        generatePassword();
+      }
+    }
+  
   return "Password here";
 }
 
