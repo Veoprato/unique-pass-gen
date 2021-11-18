e64c8427dafc0f4603a327294f3627090c2ae263
Veoprato <75872616+Veoprato@users.noreply.github.com>
2021-11-18 07:33:47 -0500

    cleaned up test console.logs; added comments to script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,31 +1,36 @@
-// Assignment Code
+// Character Arrays
 const charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const charLower = "abcdefghijklmnopqrstuvwxyz";
 const charNumber = "1234567890";
 const charSpecial = "!#$%&'()*+-./:;<=>?@[]^_`{|}~";
 
+// Generate Password Function
 function generatePassword(){
+  // Local vars for password
   var passwordLength = "";
   var characters = "";
   var generatedPassword = "";
   
+  // Password criteria
   passwordLength = window.prompt("How many characters would you like your password to be? (Must be 8-128 Characters)");
+    // if password length is invalid
   while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)){
       passwordLength = window.prompt("Not a valid character count! Please try again. (Must be 8-128 Characters)");
   }
+
   const confirmUppercase = window.confirm("Click OK to include uppercase letters.");
   const confirmLowercase = window.confirm("Click OK to include lowercase letters.");
   const confirmNumber = window.confirm("Click OK to include numbers.");
   const confirmSpecial = window.confirm("Click OK to include special characters.");
 
-  console.log(confirmUppercase, confirmLowercase, confirmNumber, confirmSpecial);
-
+  // if none of the criteria are selected
   var characterOptions = confirmUppercase + confirmLowercase + confirmNumber + confirmSpecial;
   if (characterOptions === 0) {
     window.alert("Select at least one condition for password! Try again.");
     generatePassword();
   }
   
+  // selected criteria adds arrays to local var characters
   if (confirmUppercase) {
     characters = characters.concat(charUpper);
   }
@@ -39,6 +44,7 @@ function generatePassword(){
     characters = characters.concat(charSpecial);
   }
 
+    // loops through each selected criteria randomly
   for (i = 0; i < passwordLength; i++) {
     var index = Math.floor(Math.random() * characters.length);
      generatedPassword += characters[index];
